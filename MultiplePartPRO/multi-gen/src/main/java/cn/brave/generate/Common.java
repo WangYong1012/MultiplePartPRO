4c5905c1c4efa2dc8fc4d30503c9cc1349f1acd8
apMing <1083072714@qq.com>
2017-11-06 14:36:32 +0800

    生成代码模块

--- MultiplePartPRO/multi-gen/src/main/java/cn/brave/generate/Common.java
+++ MultiplePartPRO/multi-gen/src/main/java/cn/brave/generate/Common.java
@@ -1,4 +1,26 @@
 package cn.brave.generate;
 
-public class Common {
+import org.mybatis.generator.api.ShellRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.*;
+import java.net.URL;
+
+/**
+ * 2017年11月6日 13:19:02
+ * 代码生成器类
+ * 加载完成参数后，可以直接执行该部分代码
+ */
+public class Common
+{
+    //主函数
+    public static void main(String[] args) throws IOException 
+    {
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+        URL url = loader.getResource("");
+        System.out.println(url.getPath());
+        String[] params = new String[]{"-configfile", url.getPath() + "generatorConfig.xml"};
+        ShellRunner.main(params);
+    }
 }
