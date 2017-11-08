3a29ded647cbd5a5e6d4c9659c564056c3f2fbdf
apMing <1083072714@qq.com>
2017-11-08 17:45:09 +0800

    删除已经提交的部分代码

--- MultiplePartPRO/multi-web/src/main/java/cn/brave/web/TestController.java
+++ MultiplePartPRO/multi-web/src/main/java/cn/brave/web/TestController.java
@@ -2,9 +2,9 @@ package cn.brave.web;
 
 import cn.brave.service.balance.IUserBalanceService;
 import cn.brave.util.DateTool;
-import cn.brave.web.anno.OwnAnnotation;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,13 +16,14 @@ public class TestController
     @Autowired(required = false)
     private IUserBalanceService userBalanceService;
     
+    
     @ResponseBody
     @RequestMapping(value = "/index")
-    @OwnAnnotation(name = "operate", value = "测试操作")
     public String test()
     {
         System.out.println("Now Time is ：" + DateTool.getHoleDate());
-    	
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setText("LocalUser -> RemoteUser");
         
         return JSONObject.toJSONString(userBalanceService.getList());
     }
